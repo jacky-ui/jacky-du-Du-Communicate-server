a3c6ea3d87f8f7883696db502cac69fd1079bd9a
jacky-ui <jacky.du.004@gmail.com>
2022-05-12 15:17:46 -0400

    able to take in front end inputs and display on back-end

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,9 +4,22 @@ const uniqid = require("uniqid");
 router.use(express.json());
 const utils = require("../utils");
 
-router.get("/login", (_req, res) => {
+// GET user data from JSON
+router.get("/", (_req, res) => {
     const usersData = utils.readUsers();
     res.status(200).json(usersData);
+});
+
+// Will take in user login POST request and make sure they match data
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    // Condition to make sure all fields are filled before response
+    if (!username && !password) {
+        return res.status(400).send("All fields are required");
+    }
+
+    console.log(username, password);
 })
 
 module.exports = router;
