be29d09fbfc330f8f93cd7bc43b98acd11af8a71
jacky-ui <jacky.du.004@gmail.com>
2022-05-15 23:04:11 -0400

    delete password from object send to front-end

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,13 +4,10 @@ const { JWT_KEY } = process.env;
 
 const authenticate = (req, res, next) => {
     if(req.headers.authorization === false) {
-        console.log(req.headers.authorization);
         return res.status(401).send("Uh-oh! Please login!");
     }
 
     const authToken = req.headers.authorization.split(".")[2];
-    console.log(authToken);
-    console.log(JWT_KEY);
 
     jwt.verify(authToken, JWT_KEY, (err, decoded) => {
         if (err === false) {
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -9,15 +9,20 @@ const authenticate = require('../middleware/authenticate');
 
 // GET request for current user information
 router.get("/:userId", authenticate, (req, res) => {
-    console.log(req.params);
-    console.log(req.headers.authorization);
-    const incomingId = req.params;
+    console.log(req.params.userId);
+    const incomingId = req.params.userId;
 
     // Compare and find id of current user vs database
     const userdataBase = utils.readUsers();
     const foundUser = userdataBase.find((user) => incomingId === user.id);
+    
 
-    res.status(200).send("Hello World");
+    // Found matching user but do not want to send over password. Take out password
+    delete foundUser["password"];
+
+    console.log(foundUser);
+
+    res.status(200).send(foundUser);
 })
 
 module.exports = router;
