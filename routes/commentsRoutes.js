994624a55324f273615094d82bb9949f2960a996
jacky-ui <jacky.du.004@gmail.com>
2022-06-01 23:31:58 -0400

    before had utils and filter for user outside of if. makes more sense to move inside because only use if no comments for that user

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -45,14 +45,13 @@ router.get("/", authenticate, (_req, res) => {
 // Front-end request to get all comments by a user
 router.get("/:id", authenticate, (req, res) => {
     const userId = req.params.id;
-    const userData = utils.readUsers();
-
-    const foundUser = userData.filter((user) => user.id === userId);
-    console.log(foundUser[0].profile);
-
+    
     const userComments = utils.readComments();
     const foundComments = userComments.filter((comment) => comment.id === userId);
+    
     if(foundComments.length === 0) {
+        const userData = utils.readUsers();
+        const foundUser = userData.filter((user) => user.id === userId);
         const noUserComments = [{
             message: "No posts yet!",
             profile: foundUser[0].profile
